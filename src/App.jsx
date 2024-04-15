7a26cf7c53d088eebdcb322f99528d18aeb5277c
Gaurav2418 <123626617+Gaurav2418@users.noreply.github.com>
2024-04-15 23:25:44 +0530

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,21 @@
-import "./App.css";
+import About from "./components/About";
+import Contact from "./components/Contact";
+import Home from "./components/Home";
+import Navbar from "./components/Navbar";
+import Skills from "./components/Skills";
+import Work from "./components/Work";
+
 
 function App() {
   return (
-    <main className="flex justify-center gap-4 flex-col min-h-screen">
-      <h1 className="text-3xl text-center font-bold underline">React & Tailwind CSS Starter Pack</h1>
-      <p className="text-center text-xl">This is a starter pack for React & Tailwind CSS projects.</p>
-      <img src="https://bit.ly/3wsmzTy" alt="meme" className="mx-auto" />
-    </main>
+    <div>
+      <Navbar />
+      <Home />
+      <About />
+      <Skills />
+      <Work />
+      <Contact />
+    </div>
   );
 }
 
---
+++ src/components/Work.jsx
@@ -0,0 +1,37 @@
+import React from 'react';
+
+
+
+const Work = () => {
+
+    // projects file
+    
+    //setProject(data);
+  
+  return (
+    <div name='work' className='w-full md:h-screen text-gray-300 bg-[#0a192f]'>
+      <div className='max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full'>
+        <div className='pb-8'>
+          <p className='text-4xl font-bold inline border-b-4 text-gray-300 border-pink-600'>
+            Work
+          </p>
+          <p className='py-6'>// Check out some of my recent work</p>
+          <h5>Stay tunned, this section is comming soon</h5>
+        </div>
+
+{/* container for projects */}
+<div className="grid sm:grid-cols-2 md:grid-cols-3 gap-4">
+          
+          {/* Gird Item */}
+         
+         
+ 
+
+
+</div>
+      </div>
+    </div>
+  );
+};
+
+export default Work;
